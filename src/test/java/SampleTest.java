3937cd027bb65a9f30fe4adb6dc2c34affa134fb
Andrey <ttydragon@gmail.com>
2016-03-28 16:24:00 +0300

    commit test

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by chrome on 3/28/16.
+ */
+public class SampleTest {
+    @Test
+    public void test() throws Exception {
+        assertTrue(1 == 2);
+    }
+}
